e7bc9703b1e35f4d3209c711066024f573c7d24e
gigabrek <brekgiga@gmail.com>
2026-08-28 11:05:56 +0200

    succesful conn w tr212 using demo api

--- backend/exchange.py
+++ backend/exchange.py
@@ -1,6 +1,18 @@
 import requests as rq
 import os
+from dotenv import load_dotenv
 
-authorization = {
-    "apiKey": os.getenv("ENCRYPTION_KEY")
-}
+load_dotenv()
+
+url = "https://demo.trading212.com/api/v0/equity/account/summary"
+
+apiId = os.getenv("TR212_API_ID")
+apiKey = os.getenv("TR212_API_KEY")
+
+response = rq.get(url,auth=(apiId, apiKey))
+
+if response.status_code == 200:
+    data=response.json()
+    print(data)
+else:
+    print(f"Err {response.status_code}")
